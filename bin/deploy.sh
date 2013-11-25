dc283925749dc2bf72f4f1319d0e4f9bb26b2b6b
Michal Papis <mpapis@gmail.com>
2013-11-25 15:45:29 +0100

    use full paths

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,27 +6,25 @@ release_marker="$( date +%s )"
 
 source /usr/local/rvm/scripts/rvm
 
-cd "$base_path"
-
-mkdir -p shared releases
+mkdir -p "$base_path/shared" "$base_path/releases"
 
 if
-  [[ -d shared/clone/.git ]]
+  [[ -d "$base_path/shared/clone/.git" ]]
 then
   (
-    builtin cd shared/clone
-    git remote set-url origin $base_url
+    builtin cd "$base_path/shared/clone"
+    git remote set-url origin "$base_url"
     git pull
   )
 else
-  if [[ -d shared/clone ]]
-  then rm -rf shared/clone
+  if [[ -d "$base_path/shared/clone" ]]
+  then rm -rf "$base_path/shared/clone"
   fi
-  git clone $base_url shared/clone
+  git clone "$base_url" "$base_path/shared/clone"
 fi
 
-cp -r shared/clone releases/$release_marker &&
-cd releases/$release_marker &&
+cp -r "$base_path/shared/clone" "$base_path/releases/$release_marker" &&
+cd "$base_path/releases/$release_marker" &&
 gem install --file Gemfile &&
 NOEXEC_DISABLE=1 RUBYGEMS_GEMDEPS=- nanoc compile &&
 ln -nfs "$base_path/releases/$release_marker" "$base_path/current" &&
